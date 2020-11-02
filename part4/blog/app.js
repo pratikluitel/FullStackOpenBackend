19164f7a599129a680e3f90bcb750897ab4cf352
Pratik Luitel <073bex426.pratik@pcampus.edu.np>
2020-11-02 09:53:40 +0545

    added some middleware

--- part4/blog/app.js
+++ part4/blog/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
+const middleware = require("./utils/middleware");
 const config = require("./utils/config");
 const blogsRouter = require("./controllers/blogs");
 
@@ -14,7 +15,11 @@ mongoose.connect(config.MONGODB_URI, {
 
 app.use(cors());
 app.use(express.json());
+app.use(middleware.requestLogger);
 
 app.use("/api/blogs", blogsRouter);
+
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
 
 module.exports = app;
