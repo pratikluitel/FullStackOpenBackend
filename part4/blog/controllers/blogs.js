89bd0fbf6822b591a70f80419f7ff2f1c832b0f4
Pratik Luitel <073bex426.pratik@pcampus.edu.np>
2020-11-07 21:59:40 +0545

    more tests added

--- part4/blog/controllers/blogs.js
+++ part4/blog/controllers/blogs.js
@@ -1,18 +1,19 @@
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
 
-blogsRouter.get("/", (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
+blogsRouter.get("/", async (request, response) => {
+  const blogs = await Blog.find({});
+  response.json(blogs);
 });
 
-blogsRouter.post("/", (request, response) => {
+blogsRouter.post("/", async (request, response) => {
+  if (!request.body.title || !request.body.url) {
+    return response.status(400).end();
+  }
   const blog = new Blog(request.body);
 
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
+  const savedBlog = await blog.save();
+  response.status(201).json(savedBlog);
 });
 
 module.exports = blogsRouter;
--- part4/blog/tests/blogs_api.test.js
+++ part4/blog/tests/blogs_api.test.js
@@ -18,7 +18,7 @@ test("blogs are returned as json", async () => {
     .get("/api/blogs")
     .expect(200)
     .expect("Content-Type", /application\/json/);
-});
+}, 30000);
 
 test("there are right number of blogs", async () => {
   const blogs = await helper.blogsinDb();
@@ -29,6 +29,55 @@ test("there are right number of blogs", async () => {
 test("unique identifier is named id", async () => {
   const blogs = await helper.blogsinDb();
   expect(blogs[0].id).toBeDefined();
+});
+
+test("post request creates a new blog", async () => {
+  const newBlog = {
+    title: "Fantano",
+    author: "Really",
+    url:
+      "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+    likes: 10,
+  };
+
+  await api
+    .post("/api/blogs")
+    .send(newBlog)
+    .expect(201)
+    .expect("Content-Type", /application\/json/);
+
+  const blogs = await helper.blogsinDb();
+  const urls = blogs.map((r) => r.url);
+
+  expect(blogs).toHaveLength(helper.initialBlogs.length + 1);
+  expect(urls).toContain(
+    "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll"
+  );
+});
+
+test("if likes property does not exist, default value to 0", async () => {
+  const newBlog = {
+    title: "Arthur",
+    author: "Beowulf",
+    url: "https://dummyurl.co.uk",
+  };
+
+  const response = await api
+    .post("/api/blogs")
+    .send(newBlog)
+    .expect(201)
+    .expect("Content-Type", /application\/json/);
+
+  expect(response.body.likes).toBe(0);
+});
+
+test("if properties are missing from post request, bad request", async () => {
+  const newBlog = {
+    author: "Garry",
+    likes: 5,
+  };
+
+  await api.post("/api/blogs").send(newBlog).expect(400);
 });
 
 afterAll(() => {
